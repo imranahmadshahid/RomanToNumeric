53fba472553b3fe0c448e7e9ceabcc81a8c64c60
Imran Ahmad Shahid <imranahmadshahid1@gmail.com>
2021-05-08 06:48:53 +0300

    enable swagger

--- Startup.cs
+++ Startup.cs
@@ -42,10 +42,10 @@ namespace RomanToNumericMicroservice
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RomanToNumericMicroservice v1"));
-            }
 
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RomanToNumericMicroservice v1"));
             app.UseRouting();
 
             app.UseAuthorization();
